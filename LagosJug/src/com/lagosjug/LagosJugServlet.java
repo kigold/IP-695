9c6c6a344244271005f921f7b85d02d1921d8dac
Stephen <jaidadelahuya@gmail.com>
2016-08-07 22:50:29 +0100

    next commit

--- LagosJug/src/com/lagosjug/LagosJugServlet.java
+++ LagosJug/src/com/lagosjug/LagosJugServlet.java
@@ -8,6 +8,6 @@ public class LagosJugServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws IOException {
 		resp.setContentType("text/plain");
-		resp.getWriter().println("Hello, Folks");
+		resp.getWriter().println("<h1>Welcome to LagosJUG</h1><p><a href='/project-ta-690.html'>Click here for project info.</a></p>");
 	}
 }
